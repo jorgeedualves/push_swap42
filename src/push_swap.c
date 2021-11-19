75b05e230baef92acd88aeb1726c07c946d67f0a
Jorge Alves <jorgeedualves@gmail.com>
2021-11-19 19:35:03 -0300

    ARGV included in the stack

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,23 +6,22 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 20:21:23 by joeduard          #+#    #+#             */
-/*   Updated: 2021/11/19 12:42:37 by joeduard         ###   ########.fr       */
+/*   Updated: 2021/11/19 19:33:02 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
-
-void	insertion_sort(char *array)
+#include <string.h>
+/*
+void	insertion_sort(char *stack_a)
 {	
 	int		i;
 	int		value;
-	char	*stack_b;
-	char	*stack_a;
+	int		*stack_b;
 	int		stack_size;
 
-	stack_a = array;
-	stack_size = malloc ((int) sizeof (array));
+	stack_size = malloc ((int) sizeof (stack_a));
 	i = 1;
 	while (i < stack_size - 1)
 	{		
@@ -35,15 +34,33 @@ void	insertion_sort(char *array)
 		}
 		i++;
 	}
+	printf("Lista ordenada: %d\n", *stack_a);
 }
-
-int	main(int argc, char *argv[])
+*/
+int	main(int argc, char const **argv)
 {
-	int	stack_size;
+	char	*stack;
+	int 	i;
+	int		j;
 
-	if (argc != 2)
-		return (-1);
-	printf("stack %s", *argv[1]);
-	insertion_sort(argv[1]);
+	i = 0;
+	j = 1;
+	stack = (char *)malloc((strlen(*argv) - 1) * sizeof(char));
+	if (argc < 2)
+		printf("Número invalido de argumentos!\n");
+		return (EXIT_FAILURE);
+	while (*argv)
+	{
+		if (j > 1)
+			stack[i++] = **argv;
+		argv++;
+		j++;
+	}
+	while (*stack)
+	{
+		printf("%c ", *stack);
+		stack++;
+	}
+	printf("\n");
 	return (0);
 }
