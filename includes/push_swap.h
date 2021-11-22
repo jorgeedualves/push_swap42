5482e1e5b1ab921e3ac5c2c28df0332f922517a7
Jorge Alves <jorgeedualves@gmail.com>
2021-11-22 13:54:01 -0300

    include push_swap.h

---
+++ includes/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 13:49:46 by joeduard          #+#    #+#             */
+/*   Updated: 2021/11/22 13:52:47 by joeduard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include "../libft/libft.h"
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+
+
+# endif
