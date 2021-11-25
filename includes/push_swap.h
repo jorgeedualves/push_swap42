74b21331c93d7320c5be69d8001547c21f440eb7
Jorge Alves <jorgeedualves@gmail.com>
2021-11-24 23:03:36 -0300

    insertion_sort.c test

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,14 +6,14 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 13:49:46 by joeduard          #+#    #+#             */
-/*   Updated: 2021/11/22 16:27:44 by joeduard         ###   ########.fr       */
+/*   Updated: 2021/11/23 20:41:20 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include "../libft/libft.h"
+# include "../libft/libft.h"
 # include <stdlib.h>
 # include <stdio.h>
 # include <string.h>
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,55 +6,28 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 20:21:23 by joeduard          #+#    #+#             */
-/*   Updated: 2021/11/22 18:16:02 by joeduard         ###   ########.fr       */
+/*   Updated: 2021/11/24 17:10:22 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-/*
-void	insertion_sort(char *stack, int *stack_size)
-{								
 
-	int i;
-	int value;
-	int hole;
-
-	i = 1;
-								
-	while(i < *stack_size - 1)							
-	{							
-		value = stack[i];						
-		hole = i;												
-		while(hole > 0 && stack[hole - 1] > value)						
-		{						
-			stack[hole] = stack[hole - 1];					
-			hole = hole - 1;					
-		}						
-		i++;						
-	}
-	printf("Lista ordenada: %s", *stack);							
-}								
-*/								
-int	main(int argc, char const **argv)
+/*int	main(int argc, char const **argv)
 {
-	char	*stack;
+	t_list stack_a;
+	t_list new_node;
 	int 	i;
 	int		j;
-	int		stack_size;
 
 	i = 0;
 	j = 1;
-	stack = (char *)malloc((strlen(*argv) - 1) * sizeof(char));
-	stack_size = strlen(stack);
-	if (argc < 2)
-	{
-		printf("Número invalido de argumentos!\n");
-		return (EXIT_FAILURE);
-	}
+	
+	if(agc == 2)
+		stack_a->content = ft_lstnew(*argv);
 	while (*argv)
 	{
 		if (j > 1)
-			stack[i++] = **argv;
+			stack_a->content = ft_lstadd_back(**argv, new_node);
 		argv++;
 		j++;
 	}
@@ -63,7 +36,47 @@ int	main(int argc, char const **argv)
 		printf("%c ", *stack);
 		stack++;
 	}
-	printf("\nStack Size: %d \n", stack_size);
 	printf("\n");
+	return (0);
+}
+*/
+void print_stack(t_list *stack)
+{
+	t_list *node;
+
+	node = stack;
+	while (node->next != NULL)
+	{
+		printf("%d\n", node->content);
+		node = node->next;
+	}
+	printf("%d\n", node->content);
+}
+
+int main(int argc, char **argv)
+{
+	t_list *stack = NULL;
+	t_list *node = NULL;
+	int i;
+
+	i = 0;
+	if (argc >= 2)
+	{
+		while (argv[i])
+			i++;
+		while (argv[--i])
+		{
+			node = ft_lstnew(ft_atoi(argv[i]));
+			ft_lstadd_back(&stack, node);
+			if (i == 1)
+				break ;
+		}
+		print_stack(stack);
+	}
+	else
+	{
+		ft_putstr_fd("Error:\n", 1);
+		ft_putstr_fd("	Too few arguments provided!", 1);
+	}
 	return (0);
 }
