0c93f44fabedf2da8bba655cc98f202c66c2215e
Jorge Alves <jorgeedualves84@gmail.com>
2021-11-30 14:15:14 -0300

    Prototyping the functions in the push_swap.h file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 13:49:46 by joeduard          #+#    #+#             */
-/*   Updated: 2021/11/27 14:13:14 by joeduard         ###   ########.fr       */
+/*   Updated: 2021/11/30 14:12:50 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,34 @@ typedef struct s_stacks
     t_stack *stack_a;
     t_stack *stack_b;
 }t_stacks;
+
+// Push_Swap Actions
+void	rotate(t_node **stack);
+void	reverse_rotate(t_node **stack);
+void	swap(t_node **stack);
+void	push_b(t_stack *stack_a, t_stack *stack_b);
+void	push_a(t_stack *stack_a, t_stack *stack_b);
+
+// Call and print actions
+void	do_print_sa(t_stacks *stack);
+void	do_print_sb(t_stacks *stack);
+void	do_print_ss(t_stacks *stack);
+void	do_print_pa(t_stacks *stack);
+void	do_print_pb(t_stacks *stack);
+void	do_print_ra(t_stacks *stack);
+void	do_print_rb(t_stacks *stack);
+void	do_print_rr(t_stacks *stack);
+void	do_print_rra(t_stacks *stack);
+void	do_print_rrb(t_stacks *stack);
+void	do_print_rrr(t_stacks *stack);
+
+// Stack basic operations
+t_node	*new_node(int data);
+void	push_to_stack(t_node **stack, t_node *new_node);
+void	print_stack(t_node *stack);
+t_node	*get_bottom_element(t_node *stack);
+void	append_to_stack(t_node **stack, t_node *new);
+
+
 
 #endif
