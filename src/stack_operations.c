e3f2b274cadfe4293cd5002a13830859717a9d46
Jorge Alves <jorgeedualves84@gmail.com>
2021-12-07 12:45:07 -0300

    Updated satck_operations.c file

--- src/stack_operations.c
+++ src/stack_operations.c
@@ -6,7 +6,7 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 23:26:02 by joeduard          #+#    #+#             */
-/*   Updated: 2021/12/06 10:20:30 by joeduard         ###   ########.fr       */
+/*   Updated: 2021/12/07 12:44:16 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,27 +28,112 @@ void	push_to_stack(t_node **stack, t_node *new_node)
 	*stack = new_node;       //   head -> 5 -> NULL			head(TEMP) -> 1 -> NULL
 }
 
-void	print_stack(t_node *stack)
+void print_stacks(t_stack *stack_a, t_stack *stack_b)  
+					//chamada: por push_swap.c:  print_stacks(&stacks.stack_a, &stacks.stack_b)
 {
-	t_node	*node;
+	t_node	*node_a;			// ponteiro tipo t_node de nome node_a;
+	t_node	*node_b;			// ponteiro tipo t_node de nome node_b;
+	int		diff;				// variavel diff (Para receber a dirença entre node_a e node_b)
 
-	node = stack;
-	while (node->next != NULL)
+	if (!stack_b->head)			// Se (Não tiver stack_b)
 	{
-		printf("%d\n", node->data);
-		node = node->next;
+		print_stack_a(stack_a); // Printa a stack_a
+		printf("-----	-----\n");
+		printf("  A	  B \n");
+		return ;
 	}
-	printf("%d\n", node->data);
+	else if (!stack_a->head)	// Senao Se (Não tiver a stack_a)
+	{
+		print_stack_b(stack_b); // Printa a stack_b
+		printf("-----	-----\n");
+		printf("  A	  B \n");
+		return ;
+	}
+	diff = stack_size(*stack_a) - stack_size(*stack_b); // diff recebe (stack_a - stack_b)
+		// Aqui é usado node_a e node_b para percorrer as stack sem modifica-las e perder informações
+	node_a = stack_a->head;				// node_a aponta para o nó que head aponta da stack_a
+	node_b = stack_b->head;				// node_b aponta para o nó que head aponta da stack_b
+	while (node_a->next != NULL)		// Enquanto (campo next de node_a for diferente de Null)
+	{
+		if (diff < 0)					// Se a diferenca for menor que zero
+		{									 // significa que stack_a é menor que stack_b	
+			printf("	");				// imprime
+			diff++;						// incrementa afim que diff exita mais
+		}
+		else							//Senao
+		{
+			printf("%d	", node_a->data); // Imprime o valor do node_a
+			node_a = node_a->next;		  // avança para o proximo nó;
+		}
+		if (node_b->next != NULL)		  // Se o campo next for diferente de NULL
+		{
+			if (diff > 0)				  // Se a diferenca for menor que zero
+			{									// significa que stack_a é maior que stack_b
+				printf("\n");
+				diff--;						// decrementa afim que diff exita mais
+		
+			}
+			else							// Senao
+			{
+				printf("%d\n", node_b->data);	// imprime( valor do nó B)
+				node_b = node_b->next;			// anda para o proximo nó
+			}
+		}
+		else							  // Senao	
+			printf("%d\n", node_b->data); // Imprime (valor do nó B)
+	}
+	printf("%d	", node_a->data);	// Imprime (valor do nó A)
+	printf("%d\n", node_b->data);	// Imprime (valor do nó B)
+	printf("-----	");
+	printf("-----\n");
+	printf("A	B\n");
 }
 
-t_node	*get_bottom_element(t_node *stack)
+void print_stack_a(t_stack *stack_a)
+{
+	t_node *temp;
+	
+	temp = stack_a->head;
+		while (temp != NULL)
+	{
+		printf("%d\n", temp->data);
+		temp = temp->next;
+	}
+}
+
+void print_stack_b(t_stack *stack_b)
+{
+	t_node *temp;
+	
+	temp = stack_b->head;
+	while (temp != NULL)
+	{
+		printf("	%d\n", temp->data);
+		temp = temp->next;
+	}
+}
+
+t_node *get_bottom_element(t_node *stack)
 {
 	if (stack == NULL)
-		return (0);
+		return (0);						
 	while (stack->next != NULL)
 		stack = stack->next;
 	return (stack);
 }
+
+int stack_size(t_stack stack)
+{
+	stack.size = 0;
+	while (stack.head->next != NULL)
+	{
+		stack.size++;
+		stack.head = stack.head->next;
+	}
+	stack.size++;
+	return (stack.size);
+}
+
 
 void	append_to_stack(t_node **stack, t_node *new)
 {
@@ -59,4 +144,24 @@ void	append_to_stack(t_node **stack, t_node *new)
 		last->next = new;
 	else
 		*stack = new;
+}
+
+void	min_max(t_stack *stack_a)
+{	
+	t_node *temp;
+	
+	temp = stack_a->head;
+	stack_a->min = temp->data;
+	stack_a->max = temp->data;
+	while (temp != NULL)
+	{	
+		if(temp->data < stack_a->min)
+			stack_a->min = temp->data;
+        if(temp->data > stack_a->max)
+			stack_a->max = temp->data;
+        temp = temp->next;
+	}
+	printf("\nMenor: %d\n", stack_a->min);
+    printf("Maior: %d\n", stack_a->max);
+
 }
