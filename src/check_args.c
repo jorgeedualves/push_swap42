a149f81d0b60efd35562a59dcf7cfda03e97484c
Jorge Alves <jorgeedualves84@gmail.com>
2022-01-10 23:48:58 -0300

    check_args.c update

--- src/check_args.c
+++ src/check_args.c
@@ -6,7 +6,7 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 20:21:24 by joeduard          #+#    #+#             */
-/*   Updated: 2021/12/14 21:58:45 by joeduard         ###   ########.fr       */
+/*   Updated: 2022/01/10 23:47:57 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int	validate_args(int argc, char **argv)
 	return (0);
 }
 
-void	check_duplicates(int argc, int *num)
+void	check_duplicates(int argc, int *array_num)
 {
 	int	i;
 	int	j;
@@ -42,7 +42,7 @@ void	check_duplicates(int argc, int *num)
 		j = i + 1;
 		while (j < argc)
 		{
-			if (num[i] == num[j])
+			if (array_num[i] == array_num[j])
 				exit_error();
 			j++;
 		}
@@ -50,24 +50,24 @@ void	check_duplicates(int argc, int *num)
 	}
 }
 
-void	transform_args(int argc, char **argv, int *num)
+void	transform_args(int argc, char **argv, int *array_num)
 {
 	int	i;
 
 	i = 0;
 	while (i < argc)
 	{
-		num[i] = ft_atoi(argv[i]);
+		array_num[i] = ft_atoi(argv[i]);
 		i++;
 	}
 }
 
-void	array_is_sorted(int len, int *num)
+void	array_is_sorted(int len, int *array_num)
 {
 	int	i;
 
 	i = 0;
-	while (i < len - 1 && num[i] < num[i + 1])
+	while (i < len - 1 && array_num[i] < array_num[i + 1])
 		i++;
 	if (i == len - 1)
 	{
